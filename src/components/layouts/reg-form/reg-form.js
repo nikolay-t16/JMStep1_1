bc68a9a1463ec9f956e458cd07736fecd2f904a2
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2020-10-10 02:15:46 +0300

    checkbox submit button refactoring

--- src/components/layouts/reg-form/reg-form.js
+++ src/components/layouts/reg-form/reg-form.js
@@ -2,3 +2,5 @@ import './reg-form.scss';
 import '../../blocks/social-button/social-button';
 import '../../blocks/form-input/form-input';
 import '../../blocks/form-radiobutton/form-radiobutton';
+import '../../blocks/form-checkbox/form-checkbox';
+import '../../blocks/form-submit/form-submit';
